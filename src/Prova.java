d91f8a93063b80e840136b47769821160e6fe125
marco <marco@macbook-pro-006.homenet.telecomitalia.it>
2018-10-22 16:29:41 +0200

    wertyui

--- src/Prova.java
+++ src/Prova.java
@@ -3,7 +3,6 @@ public class Prova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		System.out.println("Ciao 3");
 		System.out.println("Ciao 5");
 		System.out.println("Ciao da me");
